595bd37720e65b93d6b992a4f59e745b75833de0
DESKTOP-0G8IKG8\yotam <yotam.reg@gmail.com>
2024-08-31 11:07:16 +0300

    timer example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <esp_timer.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+bool LedOn=0;
+long t;
+
+hw_timer_t *Timer0_Cfg = NULL;
+ 
+void IRAM_ATTR Timer0_ISR()
+{
+  LedOn = !LedOn;
+    digitalWrite(LED_BUILTIN, LedOn ? HIGH : LOW);
+   digitalWrite(GPIO_NUM_32, LedOn ? HIGH : LOW);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  pinMode(GPIO_NUM_32 ,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(LED_BUILTIN,HIGH);
+Serial.begin(9600);
+  
+    Timer0_Cfg = timerBegin(0, 800, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
+    timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
+    timerAlarmWrite(Timer0_Cfg, 1000, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms
+    timerAlarmEnable(Timer0_Cfg);
 }
+ 
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  delay(1000);
+      timerAlarmWrite(Timer0_Cfg, 1000, true);
+ delay(1000);
+      timerAlarmWrite(Timer0_Cfg, 2000, true);
+
+       delay(1000);
+      timerAlarmWrite(Timer0_Cfg, 500, true);
+
+      delay(1000);
+      timerAlarmWrite(Timer0_Cfg, 2, true);
+    delay(1000);
+   
 }
