527380ad89ef4ec63c8fbdedd11763af207cae7d
DESKTOP-0G8IKG8\yotam <yotam.reg@gmail.com>
2024-09-01 23:34:33 +0300

    find out thet if the freq of the main loop smaller then the spet loop its re starts

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,81 @@ bool LedOn=0;
 long t;
 
 hw_timer_t *Timer0_Cfg = NULL;
+hw_timer_t *Timer1_Cfg = NULL;
+
+uint64_t cnt=0,maxcnt=300;//maxcnt=50~600 fast  760~1100 slow
+/*
+void RunLeftSpeed(float speed)
+{
+
+speed>0 ? digitalWrite(GPIO_NUM_33,  HIGH) : digitalWrite(GPIO_NUM_33,  LOW); 
+
+if (speed<1)maxcnt=-1;
+else{
+
+ 
+
+maxcnt =550- constrain(abs(round(speed))+50,0,550);
+}
+};*/
  
 void IRAM_ATTR Timer0_ISR()
 {
-  LedOn = !LedOn;
+  
     digitalWrite(LED_BUILTIN, LedOn ? HIGH : LOW);
-   digitalWrite(GPIO_NUM_32,  HIGH);
-   digitalWrite(GPIO_NUM_32, LOW);
+    LedOn = !LedOn;
+  if(LedOn)maxcnt=0;
+  else maxcnt=550;
+
+
+  
+ 
+  
 }
+
+
+void IRAM_ATTR Timer1_ISR()
+{
+
+
+  if (cnt>maxcnt)
+{
+ digitalWrite(GPIO_NUM_32,  HIGH);
+ digitalWrite(GPIO_NUM_32, LOW);
+ cnt=0;
+}
+cnt++;
+  
+}
+
+
+
 void setup()
 {
   pinMode(GPIO_NUM_32 ,OUTPUT);
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
 Serial.begin(9600);
-  
-    Timer0_Cfg = timerBegin(0, 80, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
+
+
+    
+    Timer0_Cfg = timerBegin(0, 8000, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
     timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
-    timerAlarmWrite(Timer0_Cfg, 1000, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms
+    timerAlarmWrite(Timer0_Cfg, 10000, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms
     timerAlarmEnable(Timer0_Cfg);
+  
+    Timer1_Cfg = timerBegin(1, 80, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
+    timerAttachInterrupt(Timer1_Cfg, &Timer1_ISR, true);
+    timerAlarmWrite(Timer1_Cfg, 20, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms//Max Rate 3000
+    timerAlarmEnable(Timer1_Cfg);
+
+
 }
  
 
 void loop()
 {
-  delay(1000);
-      timerAlarmWrite(Timer0_Cfg, 1000, true);
- delay(1000);
-      timerAlarmWrite(Timer0_Cfg, 2000, true);
 
+ Serial.println(maxcnt);
    
 }
