12019300451c01354c0dd43c7b7f1c4edc965b82
DESKTOP-0G8IKG8\yotam <yotam.reg@gmail.com>
2024-09-03 21:16:15 +0300

    set stepper test for fastest to first resonans

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,44 @@
 
 
 bool LedOn=0;
-long t;
-
+long t=100;
+int singe =-1;
 hw_timer_t *Timer0_Cfg = NULL;
 hw_timer_t *Timer1_Cfg = NULL;
 
-uint64_t cnt=0,maxcnt=300;//maxcnt=50~600 fast  760~1100 slow
-/*
+uint64_t cnt=0,maxcnt=2;//maxcnt=50~600 fast  760~1100 slow
+
 void RunLeftSpeed(float speed)
 {
 
 speed>0 ? digitalWrite(GPIO_NUM_33,  HIGH) : digitalWrite(GPIO_NUM_33,  LOW); 
-
-if (speed<1)maxcnt=-1;
-else{
+maxcnt=constrain(map(abs(speed),0,100,900,30),30,900);
+if (speed==0)maxcnt=-1; // fully stop
+ 
 
  
 
-maxcnt =550- constrain(abs(round(speed))+50,0,550);
-}
-};*/
+
+};
  
 void IRAM_ATTR Timer0_ISR()
 {
   
     digitalWrite(LED_BUILTIN, LedOn ? HIGH : LOW);
     LedOn = !LedOn;
-  if(LedOn)maxcnt=0;
-  else maxcnt=550;
-
-
+  //if(LedOn)maxcnt=0;
+  //else maxcnt=550;
   
+  t=t+singe;
+t>0 ? digitalWrite(GPIO_NUM_33,  HIGH) : digitalWrite(GPIO_NUM_33,  LOW);  
+maxcnt=constrain(map(abs(t),0,100,500,30),30,500);//speed mode;
+//maxcnt=constrain(map(abs(t),0,100,80000,700),700,80000);//slow mode;
+
+if (abs(t)<=0.1)maxcnt=-1; // fully stop
+ 
+
+  if (t>5) singe=-1;
+  if (t<-5) singe=1;
  
   
 }
@@ -57,6 +64,7 @@ cnt++;
 
 void setup()
 {
+  pinMode(GPIO_NUM_33 ,OUTPUT);
   pinMode(GPIO_NUM_32 ,OUTPUT);
   pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
@@ -64,7 +72,7 @@ Serial.begin(9600);
 
 
     
-    Timer0_Cfg = timerBegin(0, 8000, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
+    Timer0_Cfg = timerBegin(0, 800, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
     timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
     timerAlarmWrite(Timer0_Cfg, 10000, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms
     timerAlarmEnable(Timer0_Cfg);
