a1629852bf67dd65af382fc6d1410ed4a112867c
DESKTOP-0G8IKG8\yotam <yotam.reg@gmail.com>
2024-09-07 18:17:30 +0300

    imu + speed control map to deg

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,22 @@
 # define MPU_addr 0x68
 
 #define Acc_SF   8192.0
-#define Gyro_SF   32.8
+#define Gyro_SF  32.8
 
 uint32_t i =0;
-hw_timer_t *Timer0_Cfg = NULL;
+
 float ax=0,ay=0,az=0,gx=0,gy=0,gz=0;
 float pitch=0,roll=0,yaw=0;
 float AccPitch=0,AccRoll=0;
 float GyroPitch=0,GyroRoll=0,GyroYaw=0;
 unsigned long T = 0;
 float dt = 0.005;
+
+
+hw_timer_t *Timer0_Cfg = NULL;
+hw_timer_t *Timer1_Cfg = NULL;
+
+uint64_t cnt=0,maxcnt=2;//maxcnt=50~600 fast  760~1100 slow
 
 
 void IMU_init();
@@ -34,13 +40,27 @@ digitalWrite(LED_BUILTIN,i%2==0 ? HIGH : LOW);
 
 }
 
+void IRAM_ATTR Timer1_ISR()
+{
 
+
+  if (cnt>maxcnt)
+{
+ digitalWrite(GPIO_NUM_32,  HIGH);
+ digitalWrite(GPIO_NUM_32, LOW);
+ cnt=0;
+}
+cnt++;
+  
+}
 
 
 /////////////////
 void setup() {
   // put your setup code here, to run once:
- pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(GPIO_NUM_33 ,OUTPUT);
+  pinMode(GPIO_NUM_32 ,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
   digitalWrite(LED_BUILTIN,HIGH);
   Serial.begin(9600);
 
@@ -63,6 +83,11 @@ void InitTimer()
     timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
     timerAlarmWrite(Timer0_Cfg, 2000, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms
     timerAlarmEnable(Timer0_Cfg);
+
+    Timer1_Cfg = timerBegin(1, 80, true);//clock set to 80 MHz by setting the divder to 800 the gets 100 us increments => 800/80e-6 = 100e-6
+    timerAttachInterrupt(Timer1_Cfg, &Timer1_ISR, true);
+    timerAlarmWrite(Timer1_Cfg, 20, true); // by seting the overflow to 1000 we gets 1000*100e-6 = 10 ms//Max Rate 3000
+    timerAlarmEnable(Timer1_Cfg);
 }
 void loop() {
 
@@ -73,11 +98,25 @@ AccPitch = -atan2(ay,ax)*180/PI;
 
 pitch = 0.98*(pitch+gz*dt) + 0.02*AccPitch;
 
-Serial.print(GyroPitch);
-Serial.print("  ");
-Serial.print(AccPitch);
-Serial.print("  ");
+
+Serial.print(maxcnt);
+Serial.print("    ");
 Serial.println(pitch);
+// control
+
+pitch>0 ? digitalWrite(GPIO_NUM_33,  HIGH) : digitalWrite(GPIO_NUM_33,  LOW);  
+
+if (abs(pitch)<10)maxcnt=constrain(map(abs(pitch),0,10,8000,800),800,8000);//slow mode;
+else maxcnt=constrain(map(abs(pitch),10,60,460,30),30,460);//speed mode;
+//
+
+if (abs(pitch)<0.5)maxcnt=-1; // fully stop
+
+
+
+
+
+
 //Serial.print(micros()-T);
 //
 while (micros()-T<=dt*100000)
